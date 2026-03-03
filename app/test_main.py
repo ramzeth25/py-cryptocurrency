106bf0b7c503444de179b82e78938d2cd0832177
ramzeth <ramzeth25@gmail.com>
2026-03-03 08:15:53 +0200

    Solution_3

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ from app.main import cryptocurrency_action
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test__rise_prediction(mock_get_exchange_rate_prediction:
                           MagicMock) -> None:
-    mock_get_exchange_rate_prediction.return_value = 2.15
+    mock_get_exchange_rate_prediction.return_value = 1.06
     assert (cryptocurrency_action(1)
             == "Buy more cryptocurrency")
 
@@ -15,14 +15,22 @@ def test__rise_prediction(mock_get_exchange_rate_prediction:
 @mock.patch("app.main.get_exchange_rate_prediction")
 def test__fall_prediction(mock_get_exchange_rate_prediction:
                           MagicMock) -> None:
-    mock_get_exchange_rate_prediction.return_value = 1.05
+    mock_get_exchange_rate_prediction.return_value = 0.94
     assert (cryptocurrency_action(1)
             == "Sell all your cryptocurrency")
 
 
 @mock.patch("app.main.get_exchange_rate_prediction")
-def test_do_nothing_prediction(mock_get_exchange_rate_prediction:
-                               MagicMock) -> None:
+def test_do_nothing_exact_plus_5(mock_get_exchange_rate_prediction:
+                                 MagicMock) -> None:
+    mock_get_exchange_rate_prediction.return_value = 1.05
+    assert (cryptocurrency_action(1)
+            == "Do nothing")
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_exact_minus_5(mock_get_exchange_rate_prediction:
+                                  MagicMock) -> None:
     mock_get_exchange_rate_prediction.return_value = 0.95
     assert (cryptocurrency_action(1)
             == "Do nothing")
