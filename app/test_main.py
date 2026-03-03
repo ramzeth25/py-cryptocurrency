378b1cded326e84cab80f1116f0e48f4874304a1
ramzeth <ramzeth25@gmail.com>
2026-03-03 07:46:02 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,28 @@
-# write your code here
+from unittest import mock
+from unittest.mock import MagicMock
+
+from app.main import cryptocurrency_action
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test__rise_prediction(mock_get_exchange_rate_prediction:
+                          MagicMock) -> None:
+    mock_get_exchange_rate_prediction.return_value = 2.15
+    assert (cryptocurrency_action(1)
+            == "Buy more cryptocurrency")
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test__fall_prediction(mock_get_exchange_rate_prediction:
+                          MagicMock) -> None:
+    mock_get_exchange_rate_prediction.return_value = 1.05
+    assert (cryptocurrency_action(1)
+            == "Sell all your cryptocurrency")
+
+
+@mock.patch("app.main.get_exchange_rate_prediction")
+def test_do_nothing_prediction(mock_get_exchange_rate_prediction:
+                               MagicMock) -> None:
+    mock_get_exchange_rate_prediction.return_value = 0.95
+    assert (cryptocurrency_action(1)
+            == "Do nothing")
